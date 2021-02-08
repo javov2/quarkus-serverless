39d7631f78f467505b60dfd89fb83b7cee8116a1
Jerson <jrsonv@gmail.com>
2021-02-07 22:58:14 -0500

    first steps with Quarkus POST

--- src/main/java/org/javo/backend/MainApplication.java
+++ src/main/java/org/javo/backend/MainApplication.java
@@ -2,12 +2,26 @@ package org.javo.backend;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import com.google.gson.JsonObject;
 
-@QuarkusMain
-public class MainApplication implements QuarkusApplication {
-    @Override
-    public int run(String... args) throws Exception {
-        System.out.println("Hello World");
-        return 10;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/run")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class MainApplication {
+
+    @POST
+    public Response hello() {
+        JsonObject response = new JsonObject();
+        response.addProperty("greeting", "Hello, " + "Jerson" + "!");
+        return Response.ok(response).build();
     }
+
 }
